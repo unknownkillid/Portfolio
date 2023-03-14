bd9e7b65f1d74d67b1537b67f2c4aa4519c8a708
[R*] Dante <102482809+unknownkillid@users.noreply.github.com>
2023-03-14 21:53:09 +0400

    almost done

--- logic/logic.js
+++ logic/logic.js
@@ -44,3 +44,68 @@ window.addEventListener('scroll', () => {
 
 pageAnimations();
 
+
+let flag = 1;
+let burgerButton = document.getElementById('burg-btn');
+let line1 = document.getElementById('line1');
+let line2 = document.getElementById('line2');
+let line3 = document.getElementById('line3');
+const burgermenu = document.querySelector('.navbar-links-burger');
+
+
+burgerButton.addEventListener('click', function (burger) {
+    if (flag === 1) {
+        burgermenu.classList.add('burgerAnim')
+        line1.style.transition = "1s";
+        line3.style.transition = "1s";
+        line2.style.opacity = 0;
+        line1.style.transform = "rotateZ(50deg)";
+        line1.style.top = "50%";
+        line3.style.top = "50%";
+        line2.style.transition = "1s";
+        line3.style.transform = "rotateZ(-50deg)";
+        flag = 0;
+        document.body.style.overflow = "hidden";
+    } else {
+        burgermenu.classList.remove('burgerAnim')
+        document.body.style.overflowY = "scroll";
+        line1.style.transition = "1s";
+        line3.style.transition = "1s";
+        line2.style.transition = "1s";
+        line1.style.transform = "rotateZ(0deg)";
+        line3.style.transform = "rotateZ(0deg)";
+        line1.style.top = "20%";
+        line3.style.top = "68%";
+        line2.style.opacity = 1;
+        flag = 1;
+    }
+
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 766) {
+        burgermenu.classList.remove('burgerAnim')
+        line1.style.transform = "rotateZ(0deg)";
+        line3.style.transform = "rotateZ(0deg)";
+        line1.style.top = "20%";
+        line3.style.top = "68%";
+        line2.style.opacity = 1;
+        flag = 1;
+        document.body.style.overflowY = "scroll";
+        document.body.style.overflowX = "hidden";
+    }
+});
+
+let aboutButton = document.querySelector('.read-more-button');
+const aboutCotainer = document.getElementById('aboutContainer');
+const xButton = document.querySelector('.xbtn');
+
+aboutButton.addEventListener('click', () => {
+    aboutCotainer.style.display = "block";
+    document.body.style.overflow = "hidden"
+});
+
+xButton.addEventListener('click', () => {
+    aboutCotainer.style.display = "none";
+    document.body.style.overflowY = "scroll"
+});
